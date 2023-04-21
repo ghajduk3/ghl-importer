cd5b603fbc6f1590dbb309eca0afb436fb0a64d2
ghajduk3 <gh8590@student.uni-lj.si>
2023-04-21 16:19:00 +0200

    Added endpoint for saving data to the pool

--- src/services.py
+++ src/services.py
@@ -1,5 +1,28 @@
+import logging
 import typing
+
+from src import enums
+from src import models
+
+logger = logging.getLogger(__name__)
+
+_LOG_PREFIX = "[GHL-SERVICE]"
 
 
 def save_loan_data_to_pool(raw_payload: typing.Dict) -> None:
-    pass
+    logger.info(
+        "{} Saving raw payload to the pool for processing (raw_data={}).".format(
+            _LOG_PREFIX, raw_payload
+        )
+    )
+    pool_data = models.LoanPool.objects.create(
+        loan_id=None,
+        status=enums.LoanDataPoolStatus.IN_PROCESS.value,
+        status_name=enums.LoanDataPoolStatus.IN_PROCESS.name,
+        payload=raw_payload,
+    )
+    logger.info(
+        "{} Saved raw payload to the pool (pool_id={}).".format(
+            _LOG_PREFIX, pool_data.id
+        )
+    )
